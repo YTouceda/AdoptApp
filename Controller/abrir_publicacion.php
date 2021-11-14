5b0c7fde0394200b975bf93af6fa3a88efa3311e
YTouceda <Yannicktouceda@gmail.com>
2021-11-14 18:51:40 -0300

    yannick

--- Controller/abrir_publicacion.php
+++ Controller/abrir_publicacion.php
@@ -1,6 +1,6 @@
 <?php 
 
-
+include_once 'Clases/denuncia.php';
 
 class abrir_publicacion extends Controller{
     function __construct(){
@@ -22,12 +22,47 @@ class abrir_publicacion extends Controller{
         if($objPublicacion){
             $this->view->publicacion = $objPublicacion;
             $this->view->mascota = $objPublicacion->getMascota();
+            $this->view->estado_denuncia=$this->validar();
             $this->view->render('abrir_publicacion/index');
         }else{
                 // var_dump($publicacion);
             echo 'No se encontro la publicacion.';
     }
 }
+    function eliminar(){
+        if(isset($_GET['publicacion'])){
+            //var_dump($_GET['publicacion']);
+            $this->model->eliminar($_GET['publicacion']);
+        }
+        
+        header('Location:' . constant('URL'));
+    }
+
+    function denunciar(){
+        if(isset($_POST['ID_PUBLICACION']) && isset($_POST['DESCRIPCION']) && isset($_POST['ID_TIPO_DENUNCIA']) && isset($_SESSION['id'])){
+        $objDenuncia=new Denuncia();
+        $objDenuncia->setId_publicacion($_POST['ID_PUBLICACION']);
+        $objDenuncia->setDescripcion($_POST['DESCRIPCION']);
+        $objDenuncia->setId_tipo_denuncia($_POST['ID_TIPO_DENUNCIA']);
+        $objDenuncia->setId_usuario_denunciante($_SESSION['id']);
+            $this->model->denunciar($objDenuncia);
+
+        }else{
+            echo "Error, faltan datos pillín :)";
+        }
+        header('Location:' . constant('URL'));//const url
+    }
+    function validar(){
+        if(isset($_GET['publicacion'])&& isset($_SESSION['id'])){
+            $objDenuncia=new Denuncia();
+            $objDenuncia->setId_publicacion($_GET['publicacion']);
+            $objDenuncia->setId_usuario_denunciante($_SESSION['id']);
+            $estado_denuncia=$this->model->validarDenuncia($objDenuncia);
+            return $estado_denuncia;
+        }
+
+
+    }
 }
 
 
--- Model/abrir_publicacionmodel.php
+++ Model/abrir_publicacionmodel.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once 'Clases/publicacion.php';
+include_once 'Clases/denuncia.php';
 
 class abrir_publicacionModel extends Model{
     public function __construct(){
@@ -8,7 +9,6 @@ class abrir_publicacionModel extends Model{
     }
 
     public function get($publicacion){
-        $items = [];
         try {
             
             $query = $this->db->connect()->prepare("SELECT * FROM `V_PUBLICACION` WHERE ID_PUBLICACION=:ID_PUBLICACION");
@@ -29,6 +29,7 @@ class abrir_publicacionModel extends Model{
                 $objMascota->setFotos_mascota($row['FOTO_MASCOTA']);
                 $objPublicacion->setNum_contacto_publicacion($row['NUM_CONTACTO_PUBLICACION']);
                 $objPublicacion->setFecha_alta_publicacion($row['FECHA_ALTA_PUBLICACION']);
+                $objPublicacion->setId_usuario($row['ID_USUARIO']);
                 $objPublicacion->setMascota($objMascota);
 
                 // var_dump($objPublicacion);
@@ -39,6 +40,66 @@ class abrir_publicacionModel extends Model{
             return false;
         }
     }
+
+    function eliminar($id_Publicacion){
+        try{
+        $query = $this->db->connect()->prepare("UPDATE `PUBLICACION` SET `FECHA_BAJA_PUBLICACION`= SYSDATE() WHERE `ID_PUBLICACION` = :ID_PUBLICACION ");
+        $query->execute(['ID_PUBLICACION' => $id_Publicacion]);
+        } catch (PDOException $exc) {
+            echo "Error: " . $exc->getMessage();
+            return false;
+        }
+    }
+
+    function denunciar($objDenuncia){
+        try{
+            $query = $this->db->connect()->prepare("INSERT INTO `DENUNCIA`(
+            `ID_TIPO_DENUNCIA`,
+            `ID_PUBLICACION`,
+            `ID_USUARIO_DENUNCIANTE`,
+            `DESCRIPCION`,
+            `FECHA_DENUNCIA`) 
+            VALUES 
+           (".$objDenuncia->getId_tipo_denuncia().",
+            ".$objDenuncia->getId_publicacion().",
+            ".$objDenuncia->getId_usuario_denunciante().",
+            '".$objDenuncia->getDescripcion()."',
+            SYSDATE()
+            ) ");
+            $query->execute();
+            $this->contarDenuncias($objDenuncia->getId_publicacion());
+            } catch (PDOException $exc) {
+                echo "Error: " . $exc->getMessage();
+                return false;
+            }
+    }
+    function contarDenuncias($publicacion){
+        try{
+            $query = $this->db->connect()->prepare("SELECT `ID_DENUNCIA` FROM `DENUNCIA` WHERE `ID_PUBLICACION` = ".$publicacion."");
+            $query->execute();
+            $num_filas = $query->rowCount();
+            var_dump($num_filas);
+            if($num_filas>=3){
+                $this->eliminar($publicacion);
+            }
+            } catch (PDOException $exc) {
+                echo "Error: " . $exc->getMessage();
+                return false;
+            }
+
+    }
+    function validarDenuncia($objDenuncia){
+        $query= $this->db->connect()->prepare("SELECT `ID_PUBLICACION` FROM `denuncia` WHERE `ID_USUARIO_DENUNCIANTE` = ".$objDenuncia->getId_usuario_denunciante()." AND `ID_PUBLICACION`=".$objDenuncia->getId_publicacion()." ");
+        $query->execute();
+        $num_filas = $query->rowCount();
+        if($num_filas>0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
     
 }
 
